98a60176ebca4879f527d93b165297f3de6c041a
Your Name <your.email@example.com>
2024-12-25 15:30:59 +0300

    github com

---
+++ github.py
@@ -0,0 +1,49 @@
+# def artur (name,age):
+# #     return f'{name,age} жаш'
+# # print (artur("japar",15))
+
+# def artur (surname,name,age):
+#     return f'{surname,name,age} жаш'
+# print (artur("jeenbekov","japar",15))
+
+# def artur (a,b):
+#     return a + b
+# result = artur(5,5)
+# b = artur (15, 15)
+# print(result)
+# print(b)
+
+
+# def read_data(file_name):
+#     with open (file_name, "r")as file:
+#         result = file.read()
+#         return result
+
+
+ # def get_name(self):
+#         return self.__name
+#
+#     def get_age(self):
+#         return self.__age
+#
+#     def set_pol(self, new_pol):
+#            self.__pol = new_pol
+#     def set_rost(self, new_rost):
+#         self.__rost = new_rost
+#
+#     def start(self):
+#         print(f'{self.__name}  pers is startig')
+#     def move(self):
+#         print(f"{self.__age}  pers is moving")
+#     def stop(self):
+#         print(f"{self.__pol}  pers is stoping")
+#
+#     def display_info(self):
+#          print(f"name: {self.__name}\n"
+#                f'age: {self.__age}\n'
+#                f'pol:  {self.__pol}\n'
+#                )
+#
+# pers = Gamepers('Martis','18', 'male','75','172')
+# print(pers.get_age())
+# pers.display_info()
